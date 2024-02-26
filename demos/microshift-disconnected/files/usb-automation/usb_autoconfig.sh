ab6a74aae1722d5560080f5125ab9427bdf71164
Luis Arizmendi <luis.ariz@gmail.com>
2024-02-27 00:03:36 +0100

    encryption key directory

--- demos/microshift-disconnected/files/usb-automation/usb_autoconfig.sh
+++ demos/microshift-disconnected/files/usb-automation/usb_autoconfig.sh
@@ -16,7 +16,7 @@ RHDE_AUTOMATION_DIR="rhde-automation"
 RHDE_AUTOMATION_TAR="rhde-automation.tar.gz"
 RHDE_AUTOMATION_RUN="/usr/bin/rhde_automation_run.sh"
 
-ENCRYPTION_KEY="/usr/etc/rhde_automation_encryption_key"
+ENCRYPTION_KEY="/usr/share/rhde_automation_encryption_key"
 
 TEMP_DIR="/tmp/usb-autoconfigure"
 
@@ -25,7 +25,7 @@ SIGNATURE_VERIFICATION_SCRIPT="/usr/bin/signature_verification_script.sh"
 USB_DEVICE=$(cat /tmp/last-usb)
 
 SIGNATURE_FILE="rhde-automation-signature.sha256"
-PUBLIC_KEY="/usr/etc/rhde-automation-pub.pem"
+PUBLIC_KEY="/usr/share/rhde-automation-pub.pem"
 
 ######################################
 
