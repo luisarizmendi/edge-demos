095a3f65cde08fe1fcfe7d4573760e319fd87414
Luis Arizmendi <luis.ariz@gmail.com>
2024-03-07 12:11:23 +0100

    ostree local upgrade

--- demos/microshift-disconnected/files/others/upgrades/rhde-automation/05_upgrade.sh
+++ demos/microshift-disconnected/files/others/upgrades/rhde-automation/05_upgrade.sh
@@ -2,7 +2,7 @@
 
 script_dir="$(dirname "$0")"
 repo_tar="/tmp/usb-autoconfigure/rhde/rhde-image.tar"
-repo_dir="/run/install/repo/ostree/"
+repo_dir="/run/install/repo/ostree"
 
 if [ ! -d $repo_dir ]; then
     mkdir -p  $repo_dir
@@ -11,13 +11,15 @@ fi
 echo "Getting the new image"
 tar xvf $repo_tar -C $repo_dir
 
-
-
 echo "Upgrading..."
 
 ostree pull-local $repo_dir/repo
 rpm-ostree update --preview
-rpm-ostree update -r
+rpm-ostree update
+
+echo "Rebooting..."
+sleep 5
+reboot
 
 
 
